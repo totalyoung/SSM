d71c86ee9bd61fec519ab35a82e07fbbb3b594ae
totalyoung <496832900@qq.com>
2017-12-25 20:14:10 +0800

    2017年12月25日 20:14:07

--- src/main/java/config/WebAppInitializer.java
+++ src/main/java/config/WebAppInitializer.java
@@ -1,33 +1,31 @@
 package config;
 
-import javax.servlet.ServletRegistration.Dynamic;
-
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-	//返回的带有@Configuration注解的类将会用来配置ContextLoaderListener创建的应用上下文中的bean
+	// 返回的带有@Configuration注解的类将会用来配置ContextLoaderListener创建的应用上下文中的bean,这些bean通常是驱动应用后端的中间层和数据层组件
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[]{ApplicationConfig.class};
+		return new Class<?>[] { ApplicationConfig.class };
 	}
 
-	//返回的带有@Configuration注解的类将会用来定义DispatcherServlet应用上下文中的bean。
+	// 返回的带有@Configuration注解的类将会用来定义DispatcherServlet应用上下文中的bean,如控制器、
+	// 视图解析器以及处理器映射
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[]{MVCConfig.class};
+		return new Class<?>[] { MVCConfig.class };
 	}
 
-	
-	//它会将一个或多个路径映射到DispatcherServlet上。
+	// 它会将一个或多个路径映射到DispatcherServlet上。
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
-	
-//	@Override
-//	protected void customizeRegistration(Dynamic registration){
-//		registration.setInitParameters(arg0)
-//	}
+
+	// @Override
+	// protected void customizeRegistration(Dynamic registration){
+	// registration.setInitParameters(arg0)
+	// }
 
 }
