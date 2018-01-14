31408fcca8f36d25f85bcd88fde8c33503997a7b
totalyoung <496832900@qq.com>
2018-01-14 14:01:59 +0800

    2018年1月14日 14:01:57

--- src/main/java/com/mvc/index/controller/IndexController.java
+++ src/main/java/com/mvc/index/controller/IndexController.java
@@ -7,21 +7,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.common.redis.CodeRedisKey;
-import com.common.redis.RedisUtil;
+import com.total2.Person;
 
 @Controller
 public class IndexController {
 
-
-
 	@ResponseBody
 	@RequestMapping(value = "/index/{userId}", method = { RequestMethod.GET })
-	public String SSMIndex(@PathVariable String userId, @RequestParam(value = "data", required = false) String data) {
+	public Person SSMIndex(@PathVariable String userId, @RequestParam(value = "data", required = false) String data) {
 		// requestEntity.status(HttpStatus.ACCEPTED);
-		RedisUtil.set(CodeRedisKey.ORDER_SERVICE_NO + userId, "test", 3600);
-
-		return "index";
+		// RedisUtil.set(CodeRedisKey.ORDER_SERVICE_NO + userId, "test", 3600);
+		Person p = new Person();
+		p.setAge(1);
+		p.setName("adfkjh");
+		return p;
 	}
 
 }
