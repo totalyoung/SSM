6fde0e4a3adece876ac61a9b6cf8faeca07219f0
totalyoung <496832900@qq.com>
2018-01-15 22:09:36 +0800

    2018年1月15日 22:09:34

--- src/main/java/com/mvc/index/controller/IndexController.java
+++ src/main/java/com/mvc/index/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.mvc.index.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,15 +17,18 @@ import com.total2.Person;
 @Controller
 public class IndexController {
 
-	@ResponseBody
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+	// @ResponseBody
 	@RequestMapping(value = "/index/{userId}", method = { RequestMethod.GET })
 	public ResponseEntity<Person> SSMIndex(@PathVariable String userId,
 			@RequestParam(value = "data", required = false) String data) {
 		// requestEntity.status(HttpStatus.ACCEPTED);
+		// RedisUtil.set(CodeRedisKey.ORDER_SERVICE_NO + userId, "test", 3600);
 		Person p = new Person();
 		p.setAge(1);
-		p.setName("adfkjh");
-		// RedisUtil.set(CodeRedisKey.ORDER_SERVICE_NO + userId, "test", 3600);
+		p.setName("fds");
+		logger.debug("SSMIndex");
 		return new ResponseEntity<Person>(p, HttpStatus.OK);
 	}
 
@@ -32,6 +37,7 @@ public class IndexController {
 	public Person SSMIndex2(@PathVariable String userId, @ModelAttribute Person p) {
 		// requestEntity.status(HttpStatus.ACCEPTED);
 		// RedisUtil.set(CodeRedisKey.ORDER_SERVICE_NO + userId, "test", 3600);
+
 		return p;
 	}
 
